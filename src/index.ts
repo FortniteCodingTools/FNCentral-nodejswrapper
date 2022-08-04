46020c6768692206c735797e9d2175e3754da6c4
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2022-08-04 14:00:33 +0200

    new endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import getAes from "./aes"
 import getMappings from "./mappings"
 import getHotfixes from "./hotfixes"
+import { getAssets, getAssetsFromPak } from "./assets"
 
-export { getAes, getMappings, getHotfixes }
+export { getAes, getMappings, getHotfixes, getAssets, getAssetsFromPak }
